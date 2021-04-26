19b2b9708caf161a4ae77217f4d901657e7f5527
ArFe <ArFe@users.noreply.github.com>
2021-04-26 19:08:51 -0400

    Add Boostrap manually

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const electron = require('electron')
+const electron = require('electron');
     // Module to control application life.
 const app = electron.app
     // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
